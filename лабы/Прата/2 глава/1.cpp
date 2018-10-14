2aa0a79e17a161a6d1af106dcb229e435aade5b2
Yana0208 <dnoblennaya@gmail.com>
2018-10-14 14:08:29 +0300

    Первая лаба

---
+++ лабы/Прата/2 глава/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+
+int main()
+{
+	using namespace std;
+	cout << "My name is Yana";
+	cout << endl;
+	cout << "ya.yana-pryanishnikova@yandex.ru";
+	cout << endl; 
+	system ("pause");
+	return 0;
+}
