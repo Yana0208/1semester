f8143b5a0896c027de6f5a7fe21f3e4533c1cb2a
Yana0208 <dnoblennaya@gmail.com>
2018-10-22 19:26:37 +0300

    Третья лаба

---
+++ лабы/Прата/2 глава/3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+void three()
+{
+	using namespace std;
+	cout << "Three blind mise ";
+	cout << endl;
+}
+void see()
+{
+	using namespace std;
+	cout << "See how they run";
+	cout << endl;
+}
+int main()
+{
+	using namespace std;
+	three();
+	three();
+	see();
+	see();
+	system("pause");
+		return 0;
+	}
