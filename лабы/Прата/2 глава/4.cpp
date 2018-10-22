29f0783e6c58bfecdec21115b1a2e7afa2814070
Yana0208 <dnoblennaya@gmail.com>
2018-10-22 20:02:10 +0300

    Четвертая лаба

--- лабы/Прата/2 глава/4.cpp
+++ лабы/Прата/2 глава/4.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 
-
-int main()
+void t(int m)
 {
-	using namespace std;
-    int celsius;
 	int fahrenheit;
-	cout << "Enter a Celsius value ";
-	cin >> celsius; 
-	const float a = 1.8;
+	using namespace std;
+	const double a = 1.8;
 	const char b = 32;
-	fahrenheit = a * celsius;
-	fahrenheit = fahrenheit + b;
-	cout << celsius;
-	cout << " degrees Celsius is ";
+	fahrenheit = a * m;
+	fahrenheit = b + fahrenheit;
+	cout << m;
+ 	cout << " degrees Celsius is ";
 	cout << fahrenheit;
-	cout << " degrees Fahrenheit .";
-	cout << endl; 
-	system ("pause");
-	return 0;
+ 	cout << " degrees Fahrenheit .";
+ 	cout << endl; 
 }
+ int main()
+ {
+ 	using namespace std;
+	int celsius;
+	cout << "Enter a Celsius value ";
+	cin >> celsius;
+	t(celsius);
+ 	system ("pause");
+ 	return 0;
+ } 
